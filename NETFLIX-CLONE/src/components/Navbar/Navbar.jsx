cace6478109ac96ac1ee6f389bce3a724a509a6d
Amrit Pramanik <pramanikamrit778@gmail.com>
2025-08-24 22:48:24 +0530

    Initial Commit

--- NETFLIX-CLONE/src/components/Navbar/Navbar.jsx
+++ NETFLIX-CLONE/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search_icom from '../../assets/search_icon.svg'
+import bell_icom from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className="navbar-left">
+        <img className='logo' src={logo} alt="netflix-logo" />
+        <ul>
+          <li>Home</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My LIst</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div className="navber-right">
+        <img src={search_icom} alt="" className='icons' />
+        <p>Children</p>
+        <img src={bell_icom} alt="" className='icons' />
+        <div className="navber-profile">
+          <img src={profile_img} alt="" className='profile' />
+          <img src={caret_icon} alt=""/>
+        </div>
+      </div>
     </div>
   )
 }
